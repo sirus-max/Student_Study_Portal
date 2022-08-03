a309948ddd761636e3a38a67450b8b4293dafb75
Pradeep Kumar <76690746+sirus-max@users.noreply.github.com>
2022-08-03 16:21:43 +0530

    Update models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -18,12 +18,12 @@ class Notes(models.Model):
         verbose_name_plural = "notes"
         
         
-class Homework(models.Model):
+class DSA(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    subject = models.CharField(max_length=50)
+    topic = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    due = models.DateTimeField()
+    solution = models.TextField()
     is_finished = models.BooleanField(default=FALSE)
     def __str__(self):
         return self.title
